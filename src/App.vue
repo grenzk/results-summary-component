c4589794f24b7e114e2bb96ed28964aab68bf68b
grenzk <74033991+grenzk@users.noreply.github.com>
2023-06-13 20:59:00 +0800

    Structure html

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,34 @@
-<script setup></script>
+<script setup>
+import AttributeItem from './components/AttributeItem.vue'
+</script>
 
 <template>
-  <h1>Hello, Vue!</h1>
+  <main>
+    <div class="card">
+      <div class="results">
+        <h1 class="title">Your Result</h1>
+        <p class="score"><span>76</span>of 100</p>
+
+        <div class="l-flex">
+          <p class="remark">Great</p>
+          <p>You scored higher than 65% of the people who have taken these tests.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="summary">
+      <h1 class="title">Summary</h1>
+
+      <div class="l-flex">
+        <AttributeItem />
+        <AttributeItem />
+        <AttributeItem />
+        <AttributeItem />
+      </div>
+
+      <button class="button">Continue</button>
+    </div>
+  </main>
 </template>
 
 <style scoped></style>
