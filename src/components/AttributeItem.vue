6ca980c5be7739cbbf6370a828356f66a9e54703
grenzk <74033991+grenzk@users.noreply.github.com>
2023-06-13 20:58:16 +0800

    Componentize attribute item

---
+++ src/components/AttributeItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="l-flex">
+    <div class="attribute is-light-red">
+      <div class="l-inline">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          fill="none"
+          viewBox="0 0 20 20"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="1.25"
+            d="M10.833 8.333V2.5l-6.666 9.167h5V17.5l6.666-9.167h-5Z"
+          />
+        </svg>
+        <h3 class="attribute-name">Reaction</h3>
+      </div>
+      <p class="attribute-score"><span>80</span> / 100</p>
+    </div>
+  </div>
+  <template />
+</template>
